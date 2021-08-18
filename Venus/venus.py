a838b78faa61bca8ea4498d603f0c82abdda019f
gomader <gh970818@hotmail.com>
2021-08-18 17:04:03 +0900

    venus 2.0

--- Venus/venus.py
+++ Venus/venus.py
@@ -1,46 +1,82 @@
 from pywinauto.application import Application
 from pywinauto import mouse
+from pywinauto.keyboard import send_keys
 from xlrd import open_workbook
-from wx import App,Frame,Dialog,EVT_BUTTON,NewIdRef,EVT_CLOSE,TextCtrl,TE_PROCESS_ENTER,Button
+from wx import App,Dialog,EVT_BUTTON,NewIdRef,EVT_CLOSE,TextCtrl,TE_PROCESS_ENTER,Button,MessageBox,OK
 from wx.lib.filebrowsebutton import FileBrowseButtonWithHistory
 from sys import exit
 
-def inputCode(windowName,fileName):
-    
-    app = Application(backend="uia").connect(title_re=windowName)
-
-    app[windowName].maximize()
-    app[windowName]["Toolbar"].CheckBox3.click()
-
-    sheet = open_workbook(fileName).sheets()[0]
-
-    for row in range(sheet.nrows):
-
-        mouse.click(coords=(1000,1000))
-
-        app[windowName]["Dialog2"].Edit0.set_edit_text(str(sheet.cell(row,1).value))
-        app[windowName]["Dialog2"].Edit2.set_edit_text(str(sheet.cell(row,2).value))
-        app[windowName]["Dialog2"].Edit3.set_edit_text(str(sheet.cell(row,3).value))
-        
-        app[windowName]["Dialog2"].Button0.click()
-
 class MyApp(Dialog):
+
+    app = None
+
+    row = 0
+
+    index = 0
+
+    sheet = None
+
+    title = "Venus"
 
     def __init__(self):
 
-        Dialog.__init__(self,None,-1,"Venus Data Entry",size=(200,200))
+        Dialog.__init__(self,None,-1,self.title,size=(200,250))
 
         self.filePath = FileBrowseButtonWithHistory(self,labelText="File",pos=(5,20),size=(180,30))
 
-        self.windowPath = TextCtrl(self,-1,"Untitled - Venus",pos=(10,70),size=(165,25),style=TE_PROCESS_ENTER)
+        self.windowPath = TextCtrl(self,-1,"Untitled.net - Venus",pos=(10,70),size=(165,25),style=TE_PROCESS_ENTER)
 
-        self.start = Button(self,-1,"START",pos=(42,120),size=(100,-1))
+        self.startFrom = TextCtrl(self,-1,"Start from (index)",pos=(10,120),size=(165,25),style=TE_PROCESS_ENTER)
+
+        self.start = Button(self,-1,"START",pos=(42,170),size=(100,-1))
         self.Bind(EVT_BUTTON,self.onStart,self.start)
 
         self.Bind(EVT_CLOSE, self.onExit)
 
     def onStart(self,event):
-        inputCode(self.windowPath.GetValue(),self.filePath.GetValue())
+        windowsName = self.windowPath.GetValue()
+        try:
+            if self.start.GetLabel() == "START":
+                self.index = int(self.startFrom.GetValue()) - 1
+        except:
+            pass
+        try:
+            self.sheet = open_workbook(self.filePath.GetValue()).sheets()[0]
+            self.row = self.sheet.nrows
+            self.SetTitle(self.title+" - {}/{}".format(self.index+1,self.row))
+            try:
+                self.app = Application(backend="uia").connect(title_re=windowsName)
+                self.app[windowsName].maximize()
+                self.app[windowsName]["Toolbar"].CheckBox3.click()
+                while self.index != self.row:
+                    mouse.click(coords=(1000,1000))
+                    try:
+                        self.app[windowsName]["Dialog2"].Edit0.set_edit_text(str(self.sheet.cell(self.index,1).value))
+                        self.app[windowsName]["Dialog2"].Edit2.set_edit_text(str(self.sheet.cell(self.index,2).value))
+                        self.app[windowsName]["Dialog2"].Edit3.set_edit_text(str(self.sheet.cell(self.index,3).value))
+                        self.app[windowsName]["Dialog2"].Button0.click()
+                        self.index += 1
+                        send_keys('^s')
+                    except:
+                        self.SetTitle(self.title+" - {}/{}".format(self.index+1,self.row))
+                        self.start.SetLabel("CONTINUE")
+                        MessageBox("Finished {}/{}.\nYou can continue.".format(self.index+1,self.row),"Error",OK)
+                        break
+                if self.index == self.row:
+                    MessageBox("All Data inputed!","Finish",OK)
+                    self.getZero()
+            except:
+                MessageBox("Venus windows does not exist!","Error",OK)
+        except:
+            MessageBox("Excel file does not exist!","Error",OK)
+
+    def getZero(self):
+        self.app = None
+        self.row = 0
+        self.index = 0
+        self.sheet = None
+        self.title = "Venus"
+        self.start.SetLabel("START")
 
     def onExit(self,event):
         self.Destroy()
